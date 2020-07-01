8885465f44c002c3dae61434d6cc05c371006840
Soudamini <soudamini.hebbar@gmail.com>
2020-07-01 23:57:14 +0530

    Get client by client id

--- src/test/java/GetClient.java
+++ src/test/java/GetClient.java
@@ -14,4 +14,16 @@ public class GetClient {
                 .then()
                 .statusCode(200);
     }
+
+    @Test
+    public void getClientById() {
+        RestAssured.baseURI = "https://dev-853759.okta.com/oauth2/v1/clients";
+        RestAssured
+                .given()
+                .log().all()
+                .header("Authorization", "SSWS 00UgR1Qm7xVZ7rJMnkQQgvnIopBc3sDzrm7Zj0wXkK")
+                .get("/0oaic060sUCpoQHep4x6")
+                .then()
+                .log().all();
+    }
 }
