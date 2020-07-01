8e19d2164803344f2898181f3a6bfb09bc4be7fe
Soudamini <soudamini.hebbar@gmail.com>
2020-07-01 23:57:33 +0530

    Print client id

--- src/test/java/CreateClient.java
+++ src/test/java/CreateClient.java
@@ -1,25 +1,29 @@
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
 import org.testng.annotations.Test;
 
 import java.io.File;
 
 public class CreateClient {
 
-    File clientdata = new File("clientdata.json");
+    final File clientdata = new File("clientdata.json");
 
     @Test
     public void createClient() {
 
         RestAssured.baseURI = "https://dev-853759.okta.com/oauth2/v1/clients";
 
-        RestAssured
+        Response response = RestAssured
                 .given()
                 .header("Authorization", "SSWS 00UgR1Qm7xVZ7rJMnkQQgvnIopBc3sDzrm7Zj0wXkK")
                 .contentType(ContentType.JSON)
                 .body(clientdata)
-                .post()
-        .then()
-        .statusCode(201);
+                .post();
+        JsonPath jsonPath = response.jsonPath();
+        String clientId = jsonPath.getString("client_id");
+        System.out.println(clientId);
+        response.then().assertThat().statusCode(201);
     }
 }
